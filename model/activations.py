ea8f84bb91bb55fee2e166ce42f8786df2f4d122
koujm <kkoujm@gmail.com>
2022-03-08 20:38:23 +0700

    Add util for getting activation fn from identifier

--- model/activations.py
+++ model/activations.py
@@ -14,3 +14,10 @@ def fast_gelu(x):
     - [Gaussian Error Linear Units (GELUs)](https://arxiv.org/abs/1606.08415)
   """
   return x * tf.math.sigmoid(1.702 * x)
+
+
+def get(identifier):
+  if "fast_gelu" == identifier:
+    return fast_gelu
+  
+  return tf.keras.activations.get(identifier)
